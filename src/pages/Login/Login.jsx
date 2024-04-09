e8724080dabe9bc73e0c7c47ce38c91f96b300e1
tofayeLL <ahmedtofayel132@gmail.com>
2024-04-10 00:03:53 +0600

    added register section

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaGithub, FaGoogle } from "react-icons/fa";
 import { FiEye } from "react-icons/fi";
 import { FiEyeOff } from "react-icons/fi";
 import { Link } from "react-router-dom";
@@ -28,17 +29,17 @@ const Login = () => {
                 </div>
 
 
-                <form onSubmit={handleLogin} className="lg:w-[80%] mx-auto space-y-8  rounded-lg ">
+                <form onSubmit={handleLogin} className="lg:w-[80%] mx-auto space-y-5  rounded-lg ">
 
                     <div>
-                        <p className="mb-1 font-medium">Your Email:</p>
-                        <input type="email" name="email" placeholder="your email" className="input input-bordered w-full" required />
+                        <p className="mb-1 font-medium">User Email</p>
+                        <input type="email" name="email" placeholder="user email" className="input input-bordered w-full" required />
                     </div>
 
                     <div >
-                        <p className="mb-1 font-medium">Your Password:</p>
+                        <p className="mb-1 font-medium"> Password</p>
                         <div className="relative">
-                            <input type={showPass ? 'text' : 'password'} name="password" placeholder="your password" className="input input-bordered w-full" required />
+                            <input type={showPass ? 'text' : 'password'} name="password" placeholder="password" className="input input-bordered w-full" required />
 
                             <span onClick={() => setShowPass(!showPass)} className="absolute top-3 right-3">{showPass ? <FiEyeOff className="text-xl" ></FiEyeOff> : <FiEye className="text-xl"></FiEye>}</span>
 
@@ -50,11 +51,25 @@ const Login = () => {
                         <button className="btn text-white text-lg w-full bg-purple-600" type="submit">Login</button>
                     </div>
 
+                    <div className="flex flex-col w-full border-opacity-50 ">
+
+                        <div className="divider">Login with social accounts</div>
+
+                    </div>
+
 
                 </form>
-                <div className="text-center ">
-                    <p className="font-semibold mt-6">Dont have an account ? <Link to={'/register'} className="text-red-500">Register</Link></p>
+                <div className="text-center lg:space-x-3 md:space-x-2">
+                    <Link><button className="btn bg-purple-600 text-white"><FaGoogle className="text-xl" ></FaGoogle> Login with Google</button></Link>
+                    <Link><button className="btn bg-purple-600 text-white"><FaGithub className="text-xl"></FaGithub>   Login with Github</button></Link>
                 </div>
+
+
+                <div className="text-center ">
+                    <p className="font-medium mt-6 text-sm">Do not have an account ?   <Link to={'/register'} className="btn-active text-purple-700 btn-link">Register</Link></p>
+                </div>
+
+
 
 
             </div>
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,80 @@
-
+import { useState } from "react";
+import { FiEye } from "react-icons/fi";
+import { FiEyeOff } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Register = () => {
+
+    const [showPass, setShowPass] = useState(false);
+
+
+
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        console.log('click register')
+    }
+
+
+
     return (
-        <div>
-            <h1>I am from register page</h1>
-            
+        <div className="flex flex-col justify-center items-center">
+            <div className="flex flex-col justify-center  lg:w-[100vh] mx-auto space-y-3 my-6 bg-slate-200 shadow-xl lg:px-0 px-5  lg:py-12 py-6 rounded-md">
+
+
+
+                <div className=" mx-auto text-purple-700">
+                    <h1 className="text-3xl font-bold mb-6">Please Register Now!!</h1>
+                </div>
+
+
+                <form onSubmit={handleLogin} className="lg:w-[80%] mx-auto space-y-5  rounded-lg ">
+
+                    <div >
+                        <p className="mb-1 font-medium">User Name</p>
+                        <input type="text" name="name" placeholder="user name" className="input input-bordered w-full" />
+                    </div>
+
+                    <div >
+                        <p className="mb-1 font-medium">Photo Url</p>
+                        <input type="text" name="photo" placeholder="photo url" className="input input-bordered w-full" />
+                    </div>
+
+                    <div>
+                        <p className="mb-1 font-medium">User Email</p>
+                        <input type="email" name="email" placeholder="user email" className="input input-bordered w-full" required />
+                    </div>
+
+                    <div >
+                        <p className="mb-1 font-medium"> Password</p>
+                        <div className="relative">
+                            <input type={showPass ? 'text' : 'password'} name="password" placeholder="password" className="input input-bordered w-full" required />
+
+                            <span onClick={() => setShowPass(!showPass)} className="absolute top-3 right-3">{showPass ? <FiEyeOff className="text-xl" ></FiEyeOff> : <FiEye className="text-xl"></FiEye>}</span>
+
+                        </div>
+                    </div>
+
+
+                    <div>
+                        <button className="btn text-white text-lg w-full bg-purple-600" type="submit">Register</button>
+                    </div>
+
+
+
+
+                </form>
+
+
+
+                <div className="text-center ">
+                    <p className="font-medium mt-6 text-sm">Do not have an account ?   <Link to={'/login'} className="btn-active text-purple-700 btn-link">Login</Link></p>
+                </div>
+
+
+
+
+            </div>
         </div>
     );
 };
