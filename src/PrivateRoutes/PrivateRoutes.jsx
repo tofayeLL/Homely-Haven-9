89f28375f7dc8f39b0e3807c55606c949717955b
tofayeLL <ahmedtofayel132@gmail.com>
2024-04-13 14:49:45 +0600

    added loading spinner

--- src/PrivateRoutes/PrivateRoutes.jsx
+++ src/PrivateRoutes/PrivateRoutes.jsx
@@ -3,26 +3,40 @@ import { AuthContext } from "../providers/AuthProvider";
 import { Navigate } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { useLocation } from 'react-router-dom';
+import { RotatingLines } from 'react-loader-spinner'
 
 
-const PrivateRoutes = ({children}) => {
-    const {user,  loading} = useContext(AuthContext);
+const PrivateRoutes = ({ children }) => {
+    const { user, loading } = useContext(AuthContext);
     const location = useLocation();
-  
-   
 
-    if(loading){
-        return <span className="loading loading-spinner loading-lg"></span>
+
+
+    if (loading) {
+        return <div className="flex flex-col justify-center items-center">
+            <RotatingLines
+                visible={true}
+                height="100"
+                width="200"
+                color="grey"
+                strokeWidth="5"
+                animationDuration="0.75"
+                ariaLabel="rotating-lines-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+            />
+
+        </div>
     }
-    if(user){
+    if (user) {
         return children;
     }
-    
+
     return <Navigate state={location.pathname} to={'/login'}></Navigate>
 };
 
 export default PrivateRoutes;
 
 PrivateRoutes.propTypes = {
-    children:PropTypes.node
+    children: PropTypes.node
 }
--- src/Root.jsx
+++ src/Root.jsx
@@ -1,16 +1,37 @@
 import Nav from "./pages/Shared/Nav";
 import { Outlet } from "react-router-dom";
+import { useNavigation } from "react-router-dom";
+import { RotatingLines } from 'react-loader-spinner'
 
 const Root = () => {
+    const navigation = useNavigation()
     return (
         <div className="container mx-auto lg:px-20 md:px-16 px-2 font-poppins">
             <div className="mb-10 mt-4">
-            <Nav></Nav>
+                <Nav></Nav>
             </div>
-            <Outlet></Outlet>
-           
-            
-            
+            {
+                navigation.state === "loading" ?
+                    <div>
+                        <RotatingLines
+                            visible={true}
+                            height="100"
+                            width="200"
+                            color="grey"
+                            strokeWidth="5"
+                            animationDuration="0.75"
+                            ariaLabel="rotating-lines-loading"
+                            wrapperStyle={{}}
+                            wrapperClass=""
+                        />
+
+                    </div>
+                    :
+                    <Outlet></Outlet>
+            }
+
+
+
         </div>
     );
 };
