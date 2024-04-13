71f92bd8f38f42daa3f1be3312ae2a9b95275ed9
tofayeLL <ahmedtofayel132@gmail.com>
2024-04-13 18:50:42 +0600

    finish footer section

--- src/Root.jsx
+++ src/Root.jsx
@@ -2,33 +2,42 @@ import Nav from "./pages/Shared/Nav";
 import { Outlet } from "react-router-dom";
 import { useNavigation } from "react-router-dom";
 import { RotatingLines } from 'react-loader-spinner'
+import Footer from "./pages/Shared/Footer";
 
 const Root = () => {
     const navigation = useNavigation()
     return (
-        <div className="container mx-auto lg:px-20 md:px-16 px-2 font-poppins">
-            <div className="lg:mb-2 mb-0">
+        <div className="font-poppins">
+            <div className="lg:mb-2 mb-0 container mx-auto lg:px-20 md:px-16 px-2 ">
                 <Nav></Nav>
             </div>
-            {
-                navigation.state === "loading" ?
-                    <div>
-                        <RotatingLines
-                            visible={true}
-                            height="100"
-                            width="200"
-                            color="grey"
-                            strokeWidth="5"
-                            animationDuration="0.75"
-                            ariaLabel="rotating-lines-loading"
-                            wrapperStyle={{}}
-                            wrapperClass=""
-                        />
+            <div className="container mx-auto lg:px-20 md:px-16 px-2">
+                {
+                    navigation.state === "loading" ?
+                        <div>
+                            <RotatingLines
+                                visible={true}
+                                height="100"
+                                width="200"
+                                color="grey"
+                                strokeWidth="5"
+                                animationDuration="0.75"
+                                ariaLabel="rotating-lines-loading"
+                                wrapperStyle={{}}
+                                wrapperClass=""
+                            />
 
-                    </div>
-                    :
-                    <Outlet></Outlet>
-            }
+                        </div>
+                        :
+                        <Outlet></Outlet>
+                }
+            </div>
+
+            <div>
+                <Footer></Footer>
+
+            </div>
+
 
 
 
--- src/pages/Shared/Footer.jsx
+++ src/pages/Shared/Footer.jsx
@@ -1,8 +1,35 @@
 
 const Footer = () => {
     return (
-        <div>
-            
+        <div className="bg-base-300 ">
+
+            <footer className="footer container mx-auto lg:px-20 px-2 lg:py-20 py-4 text-base-content">
+                <aside>
+                   
+                    <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
+                </aside>
+                <nav>
+                    <h6 className="footer-title">Company</h6>
+                    <a href="/" className="link link-hover">About us</a>
+                    <a href="/" className="link link-hover">Agents</a>
+                    <a href="/" className="link link-hover">Blog</a>
+                    <a href="/" className="link link-hover">Contact us</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Actions</h6>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </nav>
+                <nav>
+                    <h6 className="footer-title">Explore</h6>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </nav>
+            </footer>
+
         </div>
     );
 };
